1d61345668cab874b1828ce59b2ff743f3923b8a
Katherine Low - Vendor <vn0fsjn@m-c02rd032g8wm.local>
2018-04-14 12:19:17 -0700

    Initial Commit

---
+++ Meal Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meal Tracker
+//
+//  Created by Katherine Low - Vendor on 4/14/18.
+//  Copyright © 2018 badumtsh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
